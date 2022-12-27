549e2ffcee8fef102ac920f8554cd5136339ed94
Roberto Jesús González Carrato Pozo <rjglez@MacBook-Pro-de-Roberto.local>
2022-12-27 12:07:53 +0100

    First commit

---
+++ src/main/java/com/roberto/backend/challenge/books/catalog/BackendChallengeBooksCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.roberto.backend.challenge.books.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendChallengeBooksCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendChallengeBooksCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roberto/backend/challenge/books/catalog/BackendChallengeBooksCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roberto.backend.challenge.books.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendChallengeBooksCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
