b59f8a35b974d2454dfce677ee1949fceaf0f7c9
roberjglez <roberinfor94@gmail.com>
2022-12-29 20:15:12 +0100

    Fixing a test

--- src/test/java/com/roberto/backend/challenge/books/catalog/infrastructure/exceptions/ExceptionsHandlerTest.java
+++ src/test/java/com/roberto/backend/challenge/books/catalog/infrastructure/exceptions/ExceptionsHandlerTest.java
@@ -12,11 +12,11 @@ import org.springframework.http.ResponseEntity;
 public class ExceptionsHandlerTest {
 
   @Test
-  public void shouldReturnHttpStatusBadRequestWhenThrownBookAlreadyExistsException() {
+  public void shouldReturnHttpStatusConflictWhenThrownBookAlreadyExistsException() {
     final ResponseEntity<Object> response = ExceptionsHandler.handlerBookAlreadyExistsException(new BookAlreadyExistsException(UUID.randomUUID()));
     Assertions.assertThat(response).isNotNull();
     Assertions.assertThat(response.getBody()).isNotNull();
-    Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CONFLICT);
   }
 
   @Test
